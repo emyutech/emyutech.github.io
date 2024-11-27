f94de377f0e30e0b2d25d9fd429fadcdfd38b4cc
emyutech <Emiliayuja@hotmail.com>
2024-11-27 02:15:30 -0800

    Added HashRouter for dynamic pages

--- emyuweb/src/App.jsx
+++ emyuweb/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, createBrowserRouter, createRoutesFromElements,RouterProvider } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import React from 'react'
 import './App.css'
 import Homepage from './pages/Homepage'
@@ -8,17 +8,18 @@ import Blog from './pages/Blog';
 import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path='/' element={<MainTemplate/>}>
-      <Route index element={<Homepage/>}/>
-      <Route path='/about' element={<About/>}/>
-      <Route path='/blog' element={<Blog/>}/>
-      <Route path='*' element={<NotFoundPage/>}/>
-      
-      </Route>  
-       ));
-  return <RouterProvider router={router}/>;
+  return (
+  <Router>
+    <Routes>
+    <Route path='/' element={<MainTemplate/>}>
+    <Route index element={<Homepage/>}/>
+    <Route path='about' element={<About/>}/>
+    <Route path='blog' element={<Blog/>}/>
+    <Route path='*' element={<NotFoundPage/>}/>
+    </Route>
+    </Routes> 
+  </Router>
+   );
 };
 
 export default App
